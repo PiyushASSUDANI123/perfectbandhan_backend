cccb0fe39d2fb95ae924d537ae5712bb4bd1c7cd
Piyush Assudani <piyushassudani96@gmail.com>
2026-06-24 17:13:21 +0530

    Fix whatsapp web version caching and ignore firebase key

--- src/services/whatsapp.service.js
+++ src/services/whatsapp.service.js
@@ -18,6 +18,10 @@ class WhatsAppService {
         puppeteer: {
           headless: true,
           args: ['--no-sandbox', '--disable-setuid-sandbox']
+        },
+        webVersionCache: {
+          type: 'remote',
+          remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
         }
       });
 
